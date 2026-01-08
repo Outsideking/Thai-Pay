1f07557aad57f1dad1f2f7795c1f9df535ecb821
Rufio Dinoto(Musk) <rufiodinoto244@gmail.com>
2026-01-08 13:16:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from core.payment import pay
+from core.report import get_report
 
+app = FastAPI(title="ThaiPay")
+
+@app.post("/pay")
+def payment(amount: float):
+    return pay(amount)
+
+@app.get("/report")
+def report():
+    return get_report()
