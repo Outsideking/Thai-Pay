b8742cfd74ec1ab1b2fffe6e6997953fa20892ef
Rufio Dinoto(Musk) <rufiodinoto244@gmail.com>
2026-01-08 13:17:50 +0700

    Update accounting.py

--- core/accounting.py
+++ core/accounting.py
@@ -1,1 +1,22 @@
+import json
+from datetime import datetime
 
+LEDGER_FILE = "data/ledger.json"
+
+def record_ledger(amount: float):
+    entry = {
+        "time": datetime.utcnow().isoformat(),
+        "amount": amount,
+        "type": "credit"
+    }
+
+    try:
+        with open(LEDGER_FILE, "r") as f:
+            data = json.load(f)
+    except:
+        data = []
+
+    data.append(entry)
+
+    with open(LEDGER_FILE, "w") as f:
+        json.dump(data, f, indent=2)
