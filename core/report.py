f3d51223d1a0ac4d8713c084602c3686f9f3b80f
Rufio Dinoto(Musk) <rufiodinoto244@gmail.com>
2026-01-08 13:18:21 +0700

    Update report.py

--- core/report.py
+++ core/report.py
@@ -1,1 +1,16 @@
+import json
 
+def get_report():
+    try:
+        with open("data/ledger.json", "r") as f:
+            data = json.load(f)
+    except:
+        data = []
+
+    total = sum(x["amount"] for x in data)
+
+    return {
+        "total_transactions": len(data),
+        "total_amount": total,
+        "data": data
+    }
